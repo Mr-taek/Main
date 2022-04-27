52679e26637bfa0d35a5090d5d601a560cd50ce1
taek <dlrmsxor9856@gmail.com>
2022-04-27 12:21:48 +0900

    sad

--- 판다스/text.py
+++ 판다스/text.py
@@ -1,3 +1,22 @@
 import pandas as pd
+import numpy as np
+def bmi2(size):
+    h=np.random.randint(150,200,size=size)
+    w=np.random.randint(80,120,size=size)
+    bmi=w/(h/100)**2
+    bmiCopy=np.copy(bmi)
+    bmi=bmi.astype(np.str0)
+    print(bmi)
+    for id,value in enumerate(bmiCopy):
+        print(id,value)
+        if value<18.5:  bmi[id]="thin"
+        elif value<25: bmi[id]="normal"
+        else : bmi[id]="fat"
+    data=pd.DataFrame({"height":h,"weight":w,"bmi":bmi})
+        
+    return data
 
-o=pd.DataFrame(data={'c':1,'a':5})
+data=bmi2(20)
+d=np.array([[1,256,52],[12,"s",3.23]])
+
+print(d)
